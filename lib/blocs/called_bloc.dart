4022ee50775793e5355d43f7fc200c05cd9b0d08
rafa160 <rafaelpaz86@gmail.com>
2021-07-12 17:59:19 -0300

    Cria medotos no bloc.

--- lib/blocs/called_bloc.dart
+++ lib/blocs/called_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:share_it/blocs/employee_bloc.dart';
 import 'package:share_it/components/custom_toast.dart';
 import 'package:share_it/models/called_model.dart';
 import 'package:share_it/models/employee_model.dart';
@@ -11,6 +12,10 @@ class CalledBloc extends BlocBase {
   StreamController<bool> _streamLoadingController = StreamController<bool>.broadcast();
   Stream get loading => _streamLoadingController.stream;
   Sink get loadingSink => _streamLoadingController.sink;
+
+  Stream<List> myCalledStreamList;
+  List<CalledModel> myCalledItems = [];
+  StreamSubscription streamSubscription;
 
   final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
@@ -59,10 +64,56 @@ class CalledBloc extends BlocBase {
       CustomToast.fail('erro ao fechar o chamado');
     }
   }
+  
+  Stream<List> getMyCalledList({EmployeeBloc employeeBloc}) {
+    if(employeeBloc.user.status.index != 1) {
+      return myCalledStreamList = _fireStore.collection('called_request')
+        .where('company_id', isEqualTo: employeeBloc.user.companyId)
+            .snapshots()
+            .map((event) =>
+                event.docs.map((e) => CalledModel.fromDocument(e)).toList());
+    } else {
+      return myCalledStreamList = _fireStore.collection('called_request')
+          .where('company_id', isEqualTo: employeeBloc.user.companyId).where('employee_email', isEqualTo: employeeBloc.user.email)
+          .snapshots()
+          .map((event) =>
+          event.docs.map((e) => CalledModel.fromDocument(e)).toList());
+    }
+  }
+
+  List<CalledModel> getCalledModelList({EmployeeBloc employeeBloc}) {
+    if (employeeBloc.user.status.index != 1) {
+      streamSubscription = _fireStore
+          .collection('called_requests')
+          .where('company_id', isEqualTo: employeeBloc.user.companyId)
+          .snapshots()
+          .listen((event) {
+        myCalledItems.clear();
+        for (final doc in event.docs) {
+          myCalledItems.add(CalledModel.fromDocument(doc));
+        }
+      });
+      return myCalledItems;
+    } else {
+      streamSubscription = _fireStore
+          .collection('called_requests')
+          .where('company_id', isEqualTo: employeeBloc.user.companyId)
+          .where('employee_email', isEqualTo: employeeBloc.user.email)
+          .snapshots()
+          .listen((event) {
+        myCalledItems.clear();
+        for (final doc in event.docs) {
+          myCalledItems.add(CalledModel.fromDocument(doc));
+        }
+      });
+      return myCalledItems;
+    }
+  }
 
   @override
   void dispose() {
     super.dispose();
     _streamLoadingController.close();
+    streamSubscription.cancel();
   }
 }
