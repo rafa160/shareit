9c0de625f13f8574f4abbcaf0d1ac08605a0dbc7
rafa160 <rafaelpaz86@gmail.com>
2021-07-15 11:40:37 -0300

    Correcao

--- lib/helpers/email_helper.dart
+++ lib/helpers/email_helper.dart
@@ -5,8 +5,8 @@ import 'package:share_it/components/custom_toast.dart';
 
 class EmailHelper {
 
-  String appName = 'lord_spfc@hotmail.com';
-  String password = 'oakraiders1986';
+  String appName = 'share_it_app@hotmail.com';
+  String password = 'Oakraiders1986';
 
   Future<bool> sendMessageToCompanyPlanZero(String plan, String receiver, String employeesNumbers, String name) async {
     final smtpServer = hotmail(appName, password);
@@ -15,7 +15,7 @@ class EmailHelper {
         ..recipients.add(receiver)
         ..subject = 'Inscrição no ShareIT'
       ..html = "<h1 style='color:green'>ShareIT App</h1> <p>&#128525;</p>\n <p>Olá, $name.\n</p> <p>Vamos lá, você escolheu o $plan.\n Para começar é preciso enviar algumas informações dos $employeesNumbers funcionarios.\n</p>"
-          "<p>Para cadastrar no $plan é necessario </p> <i> separar os funcionarios em 3 setores, Gerencia, Funcionario, Suporte</i>>\n"
+          "<p>Para cadastrar no $plan é necessario </p> <i> separar os funcionarios em 3 setores, Gerencia, Funcionario, Suporte</i>\n"
           "<ul><li>e-mail</li><li>nome completo</li><li>cargo</li></ul>\n"
           "<p>após realização do cadastro de todos os funcionarios enviaremos um e-mail com login e senha.</p>\n"
           "<p>Por favor lembrar de informar os responsaveis pelo suporte(Equipe que irá resolver os chamados).</p>";
